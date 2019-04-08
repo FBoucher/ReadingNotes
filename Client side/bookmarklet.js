9dd8ce76c728bfe4201297d4a650239f1a4a5c5e
Frank OSS <fboucher@outlook.com>
2019-04-08 17:34:14 -0400

    initial commit

---
+++ Client side/bookmarklet.js
@@ -0,0 +1,4 @@
+
+// Boormarklet to extract and send the URL to the Logic App
+
+javascript:(function(f,s,n,o){window.open(f+encodeURIComponent(s),n,o)}('http://localhost:7071/api/InitBookmark?postUrl=',window.location,'yourform','width=300,height=200'));
